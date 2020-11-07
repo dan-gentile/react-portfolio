9b4b502fa0363c85ff7cfe28e81a7234c201fa63
Dan Gentile <dangentile@ymail.com>
2020-11-07 13:12:13 -0800

    added folder structure

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./About.scss";
+
+export default function About() {
+  return <div></div>;
+}
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Projects.scss";
+
+export default function Projects() {
+  return <div></div>;
+}

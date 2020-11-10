389c22912e9de674edadbc824e79e669625d334e
Dan Gentile <dangentile@ymail.com>
2020-11-09 17:48:02 -0800

    project component built

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,53 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 import React from "react";
 import "./Project.scss";
+import { faCode, faDesktopAlt } from "@fortawesome/pro-light-svg-icons";
 
-export default function Project() {
-  return <div></div>;
+export default function Project(props) {
+  return (
+    <article className={props.className}>
+      <div className="info">
+        <h2>{props.title}</h2>
+        <ul className="links">
+          <li>
+            <FontAwesomeIcon
+              icon={faGithubAlt}
+              size="xl"
+              style={{ marginRight: "4px" }}
+            />
+            <a href={props.gitHub} target="_blank" rel="noreferrer">
+              GitHub |
+            </a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faDesktopAlt}
+              size="xl"
+              style={{ marginRight: "4px", marginLeft: "4px" }}
+            />
+            <a href={props.website} target="_blank" rel="noreferrer">
+              Website
+            </a>
+          </li>
+        </ul>
+        <div className="code">
+          <FontAwesomeIcon icon={faCode} style={{ marginRight: "4px" }} />
+          <p>{props.code}</p>
+        </div>
+        <div class="description">
+          <p>{props.description}</p>
+        </div>
+      </div>
+      <div className="thumbnail">
+        <a target="_blank" rel="noreferrer" href={props.thumbnail}>
+          <img
+            className="thumbnail-image"
+            src={props.thumbnail}
+            alt="screenshot"
+          />
+        </a>
+      </div>
+    </article>
+  );
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Project from "../../components/Project/Project";
+import PocketButlerImage from "../../Assets/pocketbutler-screen-shot.png";
 import "./Projects.scss";
 
 export default function Projects() {
   return (
     <div className="Projects">
       <Header header="projects-header" />
-      <Project />
+      <Project
+        className="three"
+        title="Pocket Butler"
+        gitHub="https://github.com/dan-gentile/pocket-butler"
+        website="pocketbutler.herokuapp.com/"
+        code="Sequelize | Node js | jQuery"
+        description="A full stack CRUD application that allows the user to create tasks that then appear on an upcoming task list and on a calendar view"
+        thumbnail={PocketButlerImage}
+      />
       <Footer footer="projects-footer" />
     </div>
   );
